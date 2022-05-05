8382a68ed434cfe58a157d6cb41271231dacdcd4
comptechric <comptechric@gmail.com>
2022-05-04 19:35:53 -0500

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/comptechric/logapp1
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,11 @@
+package logapp
+
+import "log"
+
+func LogInfo(message string) {
+	log.Printf("INFO - %v", message)
+}
+
+func LogWarning(message string) {
+	log.Printf("WARN - %v", message)
+}
